4d78767f86a104ea83a7828696cd7226aad70a35
weoz <weoz@ukr.net>
2016-07-08 19:34:56 +0300

    1_4_Add_servlet_jsp_html

---
+++ src/main/java/ua/kruart/traineeship/web/UserServlet.java
@@ -0,0 +1,15 @@
+package ua.kruart.traineeship.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**Created by kruart on 08.07.2016.*/
+public class UserServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+    }
+}
