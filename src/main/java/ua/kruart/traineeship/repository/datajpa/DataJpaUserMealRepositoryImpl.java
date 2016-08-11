fe01020badbb0f05f9f5902764c573023b5227a7
weoz <weoz@ukr.net>
2016-08-11 19:20:44 +0300

    6_01_HW05_data_jpa

--- src/main/java/ua/kruart/traineeship/repository/datajpa/DataJpaUserMealRepositoryImpl.java
+++ src/main/java/ua/kruart/traineeship/repository/datajpa/DataJpaUserMealRepositoryImpl.java
@@ -1,6 +1,8 @@
 package ua.kruart.traineeship.repository.datajpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ua.kruart.traineeship.model.UserMeal;
 import ua.kruart.traineeship.repository.UserMealRepository;
 
@@ -10,28 +12,39 @@ import java.util.List;
 /**Created by kruart on 09.08.2016.*/
 @Repository
 public class DataJpaUserMealRepositoryImpl implements UserMealRepository {
+    @Autowired
+    private ProxyUserMealRepository proxy;
+
+    @Autowired
+    private ProxyUserRepository userProxy;
+
     @Override
+    @Transactional
     public UserMeal save(UserMeal userMeal, int userId) {
-        return null;
+        if (!userMeal.isNew() && get(userMeal.getId(), userId) == null) {
+            return null;
+        }
+        userMeal.setUser(userProxy.getOne(userId));
+        return proxy.save(userMeal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return proxy.delete(id, userId) != 0;
     }
 
     @Override
     public UserMeal get(int id, int userId) {
-        return null;
+        return proxy.get(id, userId);
     }
 
     @Override
     public List<UserMeal> getAll(int userId) {
-        return null;
+        return proxy.getAll(userId);
     }
 
     @Override
     public List<UserMeal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return proxy.getBetween(startDate, endDate, userId);
     }
 }
