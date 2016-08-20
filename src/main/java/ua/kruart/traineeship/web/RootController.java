271ab99baf7bb9e821e6a537da824d2948e5d79d
weoz <weoz@ukr.net>
2016-08-20 16:11:53 +0300

    6_10_spring_webmvc

---
+++ src/main/java/ua/kruart/traineeship/web/RootController.java
@@ -0,0 +1,36 @@
+package ua.kruart.traineeship.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ua.kruart.traineeship.LoggedUser;
+import ua.kruart.traineeship.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**Created by kruart on 20.08.2016.*/
+@Controller
+public class RootController {
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String root() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String userList(Model model) {
+        model.addAttribute("userList", service.getAll());
+        return "userList";
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public String setUser(HttpServletRequest request) {
+        int userId = Integer.valueOf(request.getParameter("userId"));
+        LoggedUser.setId(userId);
+        return "redirect:meals";
+    }
+}
