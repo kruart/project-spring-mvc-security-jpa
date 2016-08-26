d3d95909b7a3de74e9b85c711efee83173e365fc
weoz <weoz@ukr.net>
2016-08-26 10:23:04 +0300

    8_08_ajax_datatables

---
+++ src/main/java/ua/kruart/traineeship/web/user/AdminAjaxController.java
@@ -0,0 +1,39 @@
+package ua.kruart.traineeship.web.user;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ua.kruart.traineeship.model.Role;
+import ua.kruart.traineeship.model.User;
+
+import java.util.List;
+
+/**Created by Arthur on 25.08.2016.*/
+
+@RestController
+@RequestMapping("/ajax/admin/users")
+public class AdminAjaxController extends AbstractUserController {
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<User> getAll() {
+        return super.getAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void createOrUpdate(@RequestParam("id") int id,
+                               @RequestParam("name") String name,
+                               @RequestParam("email") String email,
+                               @RequestParam("password") String password) {
+
+        User user = new User(id, name, email, password, Role.ROLE_USER);
+        if (id == 0) {
+            super.create(user);
+        } else {
+            super.update(user, id);
+        }
+    }
+}
